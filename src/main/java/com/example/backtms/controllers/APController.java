5d8beed6e260597a16bdc44708508989a4e8c39a
joseGuerrero2003 <1005935249@u.icesi.edu.co>
2024-04-24 09:21:57 -0500

    login y logout

--- src/main/java/com/example/backtms/controllers/APController.java
+++ src/main/java/com/example/backtms/controllers/APController.java
@@ -16,10 +16,31 @@ public class APController {
     @Autowired
     DoctorRepository doctorRepository;
 
-    @PostMapping("doctor/create")
+    @PostMapping("user/create")
     public ResponseEntity<?> create(@RequestBody Doctor doctor) {
         doctorRepository.save(doctor);
         return ResponseEntity.status(200).body(new GenericMessage("Usuario almacenado exitosamente"));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Doctor doctor) {
+
+        String email = doctor.getEmail();
+        String password = doctor.getPassword();
+
+        // Comparando con las credenciales correctas
+        if ("Andy@hotmail.com".equals(email) && "1234".equals(password)) {
+            return ResponseEntity.ok(new GenericMessage("Inicio de sesión exitoso"));
+        } else {
+            return ResponseEntity.status(401).body(new GenericMessage("Credenciales incorrectas"));
+        }
+    }
+
+
+    @GetMapping("user/list")
+    public ResponseEntity<?> list() {
+        var users = doctorRepository.findAll();
+        return ResponseEntity.status(200).body(users);
     }
 
 }
